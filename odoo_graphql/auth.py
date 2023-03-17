c1259e325dcce2ab1b59fe7b8bf5d7a00799c478
blake-clancyworld <103262609+blake-clancyworld@users.noreply.github.com>
2023-03-17 14:07:34 -0400

    Update auth.py

--- odoo_graphql/auth.py
+++ odoo_graphql/auth.py
@@ -1,21 +1,38 @@
-from odoo import http
-from odoo.http import request
-import json
+import odoo
+from odoo import api, registry
 
-def authenticate_and_execute(query, auth):
-    # Authenticate user
-    user = request.env['res.users'].sudo().search([('login', '=', auth['login'])])
-    if not user:
-        return {'error': 'Invalid login'}
+def authenticate_user(dbname, login, password):
+    try:
+        odoo.registry(dbname)
+    except Exception:
+        return None
+
+    with odoo.api.Environment.manage():
+        try:
+            uid = odoo.http.request.session.authenticate(dbname, login, password)
+            if uid:
+                return odoo.http.request.env.user
+        except Exception:
+            return None
+    return None
+
+def authenticate_and_execute(fn, auth):
+    dbname = odoo.http.request.session.db
+    login = auth.get("login")
+    password = auth.get("password")
+    user = authenticate_user(dbname, login, password)
     
-    if not user.check_credentials(auth['password']):
-        return {'error': 'Invalid password'}
-
-    # Set company
-    if 'company_id' in auth:
-        user.write({'company_id': int(auth['company_id'])})
-        request.env.context = dict(request.env.context, allowed_company_ids=[int(auth['company_id'])])
-
-    # Execute query
-    result = query(request, user)
-    return result
+    if user:
+        company_id = auth.get("company_id")
+        if company_id:
+            user.write({'company_id': int(company_id)})
+            user.write({'company_ids': [(4, int(company_id))]})
+        
+        return fn(odoo.http.request, user)
+    else:
+        return {
+            "data": None,
+            "errors": {
+                "message": "Authentication failed"
+            }
+        }
