6a5db43153eeb3a494c5d3af7b982c260d59f864
blake-clancyworld <103262609+blake-clancyworld@users.noreply.github.com>
2023-03-21 09:15:16 -0400

    Update resolver.py

--- odoo_graphql/resolver.py
+++ odoo_graphql/resolver.py
@@ -6,9 +6,9 @@ def resolve_relation_field(self, info, field_name, field_def):
     ids = self[field_name].ids
     env = info.context["env"]
     # Get company_id from auth
-    company_id = None
+    company_ids = None
     if 'auth' in info.context:
-        company_id = info.context['auth'].get('company_id')
+        company_ids = info.context['auth'].get('company_ids')
     # Retrieve records based on company_id
-    records = utils.retrieve_records(env, model_name, ids=ids, company_id=company_id)
+    records = utils.retrieve_records(env, model_name, ids=ids, company_ids=company_ids)
     return records
